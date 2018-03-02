7ac87b776637a5d0766ee4e2f9741b4fc93df55e
finile <wu.nile@gmail.com>
2018-03-02 16:22:12 +0800

    modified cart veiw at prduct index veiw page and product show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,15 @@ class ProductsController < ApplicationController
     @products = Product.page(params[:page]).per(12)
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def add_to_cart
     @product = Product.find(params[:id])
     current_cart.add_cart_item(@product)
 
     redirect_to root_path
   end
-
 
 end
