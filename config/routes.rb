bc49647e087482dfe70c908b706cb9700006d664
finile <wu.nile@gmail.com>
2018-03-20 14:46:45 +0800

    modified checout_spgateway html page and create spgateway_controller for return after payment

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,25 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "products#index"
+  
   resources :products, only: [:index, :show] do
     post :add_to_cart, on: :member
     post :remove_from_cart, on: :member
     post :adjust_item, on: :member
   end
+  
   resource :cart
+  
   resources :orders do
     post :checkout_spgateway, on: :member
   end
+
+  post "spgateway/return"
 
   namespace :admin do
     root "products#index"
     resources :products
     resources :orders
   end
+
 end
